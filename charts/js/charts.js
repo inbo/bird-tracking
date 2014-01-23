a2c776563280e447001a54c1c71cd7522b44d694
Bart Aelterman <bart.aelterman@gmail.com>
2014-01-23 12:54:54 +0100

    update javascript for stacked-area

--- charts/js/charts.js
+++ charts/js/charts.js
@@ -1,4 +1,8 @@
+var weekIndex = 0;
+firstWeek = weeks[weekIndex];
+subsetAreaData = subset(hour_stacked_area_data, firstWeek)
 drawHourLineChart(hour_month_linedata);
+drawHourAreaChart(subsetAreaData);
 drawDayCalHeatmap("#day-month-heatmap", day_month_heatdata);
 drawHourCalHeatmap("#hour-month-heatmap", hour_month_heatdata);
 
@@ -78,4 +82,72 @@ function drawHourLineChart(data) {
 
 	return chart;
     });
+}
+
+function drawHourAreaChart(data) {
+    nv.addGraph(function() {
+	var chart = nv.models.stackedAreaChart()
+	  .x(function(d) {return d["x"]})
+	  .y(function(d) {return d["y"]})
+	  .clipEdge(true);
+
+        chart.xAxis
+	    .axisLabel("Hour")
+	    .tickFormat(d3.format(',r'));
+
+        chart.yAxis
+	    .axisLabel('distance (km)')
+	    .tickFormat(d3.format(',f'))
+	    ;
+
+      d3.select('#stackedareachart svg')
+	    .datum(data)
+	    .transition().duration(500)
+	    .call(chart);
+
+      nv.utils.windowResize(chart.update);
+      return chart;
+    });
+}
+
+// This function will fetch a subset of values from an array.
+// The array is the one that is expected for the stacked area chart.
+// The subsKeys should contain an array of dates (in string format)
+// so that this function will only return the data points from the
+// given dates.
+function subset (array, subsKeys) {
+    outArr = new Array();
+    for (var i = 0; i < subsKeys.length ; i++) {
+	key = subsKeys[i];
+	console.log("key: " + key);
+	for (var k = 0; k < array.length; k++) {
+	    obj = array[k];
+	    objKey = obj["key"];
+	    if (objKey === key) {
+		outArr.push(obj);
+	    }
+	}
+    }
+    console.log("out array: " + outArr);
+    return outArr;
+}
+
+function getWeek(weekIndex, weeks) {
+    return weeks[weekIndex];
+}
+
+function nextWeek() {
+    if (weekIndex < weeks.length - 1) {
+	weekIndex++;
+    }
+    var newData = subset(hour_stacked_area_data, weeks[weekIndex]);
+    drawHourAreaChart(newData);
+}
+
+function previousWeek() {
+    if (weekIndex > 0) {
+	weekIndex = weekIndex - 1;
+    }
+    var newData = subset(hour_stacked_area_data, weeks[weekIndex]);
+    drawHourAreaChart(newData);
 }
