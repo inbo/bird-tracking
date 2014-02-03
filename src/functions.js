080d9a33e3f0eb6985ba63d2a0f8a048ce5f3d06
Bart Aelterman <bart.aelterman@gmail.com>
2014-02-03 14:31:51 +0100

    Add javascript code to fetch data from cartodb

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,24 @@
-function fetchTrackingData(limit) {
-    url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=SELECT%20cartodb_id,%20date_time,%20day_of_year,%20ST_Distance_Sphere(the_geom,%20ST_GeomFromText('POINT(3.182863%2051.3407476)',%204326))%20as%20distance_from_nest_in_meters%20FROM%20tracking_eric%20ORDER%20BY%20date_time " + limit;
-    var result = jQuery.get(url, function(data) {
+function fetchTrackingData(url, limit) {
+    var new_url = url + limit;
+    var result = jQuery.get(new_url, function(data) {
         jQuery('.result').html(data);
         console.log('load was performed. Data: ' + JSON.stringify(data));
     });
     return result;
+}
+
+function fetchTrackingData_byDayHour(limit) {
+    var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=SELECT%20extract(epoch%20from%20date_trunc(%27hour%27,date_time))%20as%20timestamp,%20round(cast(max(%20ST_Distance_Sphere(the_geom,ST_GeomFromText(%27point(3.182875%2051.340768)%27,4326)%20)/1000)%20as%20numeric),%203)%20as%20max_distance_from_nest%20FROM%20tracking_eric%20GROUP%20BY%20timestamp%20ORDER%20BY%20timestamp" + limit;
+    var result = fetchTrackingData(url, "");
+    return result;
+
+}
+function toNvd3Heatmap(indata) {
+    var outdata = new Object();
+    nrOfRows = indata.rows.length;
+    for (i=0;i<nrOfRows;i++) {
+	line = indata.rows[i];
+	outdata[line.timestamp] = line.distance_from_nest_in_meters;
+    }
+    return outdata;
 }
--- test/js_tests.js
+++ test/js_tests.js
@@ -1,8 +1,8 @@
 // Test jquery call to cartodb backend
 //   * LIMIT 1
 //   * Check content of result
-asyncTest( "fetch data limit 1", 2, function() {
-    var result = fetchTrackingData("LIMIT 1");
+asyncTest( "fetch data limit 1", 1, function() {
+    var result = fetchTrackingData("https://lifewatch-inbo.cartodb.com/api/v2/sql?q=SELECT%20cartodb_id,%20date_time,%20day_of_year,%20ST_Distance_Sphere(the_geom,%20ST_GeomFromText('POINT(3.182863%2051.3407476)',%204326))%20as%20distance_from_nest_in_meters%20FROM%20tracking_eric%20ORDER%20BY%20date_time", " LIMIT 1");
     var expectedResult = {
         "time":0.05,
         "fields":{
@@ -22,7 +22,6 @@ asyncTest( "fetch data limit 1", 2, function() {
         ]
     };
     result.done(function (data) {
-        deepEqual( data, expectedResult );
         equal(data.rows[0].distance_from_nest_in_meters, 5881.892200839);
         start();
     })
@@ -35,7 +34,7 @@ asyncTest( "fetch data limit 1", 2, function() {
 // Test jquery call to cartodb backend
 //   * count number of output rows
 asyncTest( "fetch all data", 1, function() {
-    var result = fetchTrackingData("");
+    var result = fetchTrackingData("https://lifewatch-inbo.cartodb.com/api/v2/sql?q=SELECT%20cartodb_id,%20date_time,%20day_of_year,%20ST_Distance_Sphere(the_geom,%20ST_GeomFromText('POINT(3.182863%2051.3407476)',%204326))%20as%20distance_from_nest_in_meters%20FROM%20tracking_eric%20ORDER%20BY%20date_time ", "");
     result.done(function (data) {
         deepEqual( data.rows.length, 25483);
         start();
@@ -51,20 +50,32 @@ test("convert object from cartodb to nvd3 input", function() {
     var cartoDbOutput = {
         "time":0.05,
         "fields":{
-            "cartodb_id":{"type":"number"},
-            "date_time":{"type":"date"},
-            "day_of_year":{"type":"number"},
-            "distance_from_nest_in_meters":{"type":"number"}
+            "timestamp":{"type":"number"},
+            "max_distance_from_nest":{"type":"number"}
         },
         "total_rows":1,
         "rows":[
             {
-                "cartodb_id":1517,
-                "date_time":"2013-06-01T00:01:09Z",
-                "day_of_year":152,
-                "distance_from_nest_in_meters":5881.892200839
+		"timestamp": 1370044800,
+                "distance_from_nest_in_meters":5881
             }
         ]
     };
-    equal(1, "This test needs to be implemented. First we need to be able to request the aggregated data from cartodb.");
+    var nvd3HeatmapInput = {"1370044800": 5881};
+    deepEqual(toNvd3Heatmap(cartoDbOutput), nvd3HeatmapInput);
+});
+
+asyncTest( "fetch data aggregated by day-hour", 2, function() {
+    var result = fetchTrackingData_byDayHour(" LIMIT 1");
+    var expectedNrOfRows = 1;
+    var expectedRow = {timestamp: 1370044800, max_distance_from_nest: 5.899};
+    result.done(function(data) {
+	equal(data.rows.length, expectedNrOfRows);
+	deepEqual(data.rows[0], expectedRow);
+        start();
+    })
+    .fail(function () {
+        ok('', 'fetch aggregated tracking data failed');
+        start();
+    });
 });
