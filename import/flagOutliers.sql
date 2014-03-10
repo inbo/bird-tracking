af2046f131f726121d3cbe879920027875a08176
Peter Desmet <peter.desmet.work@gmail.com>
2014-03-10 09:09:20 +0100

    Create flagOutliers.sql

---
+++ import/flagOutliers.sql
@@ -0,0 +1,34 @@
+-- SQL to flag outliers in the tracking data
+-- * Records later than 2014
+-- * Records with an altitude above 10km
+-- * Records with a speed above 120km/h
+
+with select_fields as (
+  select
+    t.cartodb_id,
+    d.device_info_serial,
+    d.bird_name,
+    d.tracking_start_date_time,
+    t.date_time,
+    t.altitude,
+    t.the_geom,
+    t.the_geom_webmercator,
+    (st_distance_sphere(t.the_geom,lag(t.the_geom,1) over(order by t.device_info_serial, t.date_time))/1000)/(extract(epoch from (t.date_time - lag(t.date_time,1) over(order by t.device_info_serial, t.date_time)))/3600) as km_per_hour
+  from
+    bird_tracking as t
+    left join bird_tracking_devices as d
+    on t.device_info_serial = d.device_info_serial
+)
+
+update bird_tracking as to_flag
+set userflag = true
+from (
+  select *
+  from select_fields
+  where
+    date_time > '2015-01-01'
+    -- or date_time < tracking_start_date_time
+    or altitude > 10000
+    or km_per_hour > 120
+  ) as outliers
+where outliers.cartodb_id = to_flag.cartodb_id
