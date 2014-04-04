ec33d172b7a27eae286987ab40c92bafb8e07186
Peter Desmet <peter.desmet.work@gmail.com>
2014-04-04 09:32:13 +0200

    Update table name

--- cartodb/import/removeTestRecords.sql
+++ cartodb/import/removeTestRecords.sql
@@ -1,7 +1,7 @@
 -- SQL to remove test tracking records, when tag was not mounted on bird
 
-delete from bird_tracking
+delete from bird_tracking_new_data
 using bird_tracking_devices as d
 where
-  bird_tracking.device_info_serial = d.device_info_serial
-  and bird_tracking.date_time < d.tracking_start_date_time
+  bird_tracking_new_data.device_info_serial = d.device_info_serial
+  and bird_tracking_new_data.date_time < d.tracking_start_date_time
