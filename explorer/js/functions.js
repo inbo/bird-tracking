96ba9f207d2b34689306db2aa5b7e2870a616564
Peter Desmet <peter.desmet.work@gmail.com>
2014-05-20 11:47:49 +0200

    Rename colony to tracking_start

--- explorer/js/functions.js
+++ explorer/js/functions.js
@@ -6,8 +6,8 @@ function fetchTrackingData(url, limit) {
     return result;
 }
 
-function fetchTrackingData_byDayHour(device_id, colony_position, limit) {
-    var sql = vsprintf("WITH distance_view AS (SELECT date_time, ST_Distance_Sphere(the_geom,ST_GeomFromText('%s',4326) ) AS distance_in_meters FROM bird_tracking WHERE device_info_serial='%s' AND userflag IS FALSE) SELECT extract(epoch FROM date_trunc('hour',date_time)) AS timestamp, round((max(distance_in_meters)/1000)::numeric, 3) AS distance FROM distance_view GROUP BY timestamp ORDER BY timestamp", [colony_position, device_id]);
+function fetchTrackingData_byDayHour(device_id, tracking_start_position, limit) {
+    var sql = vsprintf("WITH distance_view AS (SELECT date_time, ST_Distance_Sphere(the_geom,ST_GeomFromText('%s',4326) ) AS distance_in_meters FROM bird_tracking WHERE device_info_serial='%s' AND userflag IS FALSE) SELECT extract(epoch FROM date_trunc('hour',date_time)) AS timestamp, round((max(distance_in_meters)/1000)::numeric, 3) AS distance FROM distance_view GROUP BY timestamp ORDER BY timestamp", [tracking_start_position, device_id]);
     var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + sql + limit;
     var result = fetchTrackingData(url, "");
     return result;
@@ -57,14 +57,14 @@ function toNvd3Linedata(indata) {
 }
 
 function getAllBirdInfo(limit) {
-    var sql = "select bird_name, device_info_serial, sex, scientific_name, colony_longitude, colony_latitude from bird_tracking_devices order by bird_name";
+    var sql = "select bird_name, device_info_serial, sex, scientific_name, tracking_start_longitude, tracking_start_latitude from bird_tracking_devices order by bird_name";
     var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + sql + limit;
     var result = fetchTrackingData(url, "");
     return result;
 }
 
-function getMaxDistance(device_id, colony_lon, colony_lat) {
-    var sql = vsprintf("WITH distance_per_day as ( SELECT ST_Distance_Sphere( ST_GeomFromText('point(' || longitude || ' ' || latitude || ')',4326), ST_GeomFromText('point(%s %s)',4326)) as distance_from_colony FROM bird_tracking WHERE device_info_serial=%s AND userflag IS FALSE) SELECT MAX(distance_from_colony) FROM distance_per_day", [colony_lon, colony_lat, device_id]);
+function getMaxDistance(device_id, tracking_start_lon, tracking_start_lat) {
+    var sql = vsprintf("WITH distance_per_day as ( SELECT ST_Distance_Sphere( ST_GeomFromText('point(' || longitude || ' ' || latitude || ')',4326), ST_GeomFromText('point(%s %s)',4326)) as distance_from_start_position FROM bird_tracking WHERE device_info_serial=%s AND userflag IS FALSE) SELECT MAX(distance_from_start_position) FROM distance_per_day", [tracking_start_lon, tracking_start_lat, device_id]);
     var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + sql;
     var result = fetchTrackingData(url, "");
     return result
