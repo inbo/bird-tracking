ef84976049f1183fbfeb510bcee198cdb4b269f9
Peter Desmet <peter.desmet.work@gmail.com>
2014-08-19 14:56:59 +0200

    Remove link that does not work

--- cartodb/maintenance/selectTrackingPeriods.sql
+++ cartodb/maintenance/selectTrackingPeriods.sql
@@ -1,6 +1,5 @@
 -- SQL to show tracking days per device.
 -- A relatively low number of tracking days could indicate that the bird is dead.
--- https://lifewatch-inbo.cartodb.com/viz/13fd195c-b45d-11e3-b989-0e230854a1cb/table
 
 select
   d.bird_name,
