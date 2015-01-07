5c3e6a2e0833ed0dac394525e42d2da37e9427e9
Bart Aelterman <bart.aelterman@gmail.com>
2015-01-07 14:41:38 +0100

    fetch bird sex

--- explorer/js/app.js
+++ explorer/js/app.js
@@ -12,7 +12,7 @@ function fetchTrackingData(url) {
 
 // function to fetch all birds in the bird_tracking_devices table
 function fetchBirdData() {
-    query = "SELECT bird_name, device_info_serial, scientific_name from bird_tracking_devices";
+    query = "SELECT bird_name, device_info_serial, sex, scientific_name from bird_tracking_devices";
     var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + query;
     return fetchTrackingData(url);
 }
