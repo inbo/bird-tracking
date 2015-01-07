ff777c5487e16ce226a3c399fcc2ba6098376110
Bart Aelterman <bart.aelterman@gmail.com>
2015-01-07 14:40:37 +0100

    Add comments

--- explorer/js/app.js
+++ explorer/js/app.js
@@ -1,6 +1,8 @@
 // -------------------------
 // Fetch data from cartodb
 // -------------------------
+
+// general function to fetch data given a url
 function fetchTrackingData(url) {
     var result = jQuery.get(url, function(data) {
         jQuery('.result').html(data);
@@ -8,6 +10,7 @@ function fetchTrackingData(url) {
     return result;
 }
 
+// function to fetch all birds in the bird_tracking_devices table
 function fetchBirdData() {
     query = "SELECT bird_name, device_info_serial, scientific_name from bird_tracking_devices";
     var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + query;
