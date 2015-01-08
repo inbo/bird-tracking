faa0f7453821bb04d24d8b0015678ef702fed10f
Bart Aelterman <bart.aelterman@gmail.com>
2015-01-08 07:08:42 +0100

    Add lat lon to bird data

--- explorer/js/app.js
+++ explorer/js/app.js
@@ -19,7 +19,7 @@ function fetchDistancesByDay(device, point) {
 
 // function to fetch all birds in the bird_tracking_devices table
 function fetchBirdData() {
-    query = "SELECT bird_name, device_info_serial, sex, scientific_name from bird_tracking_devices";
+    query = "SELECT bird_name, device_info_serial, sex, scientific_name, longitude, latitude from bird_tracking_devices";
     var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + query;
     return fetchTrackingData(url);
 }
