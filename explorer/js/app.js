608939aa86048e8d63d8ea6a6b6eb31cf9268115
Bart Aelterman <bart.aelterman@gmail.com>
2015-01-14 14:04:47 +0100

    remove console.log

--- explorer/js/app.js
+++ explorer/js/app.js
@@ -28,7 +28,6 @@ function fetchDistTravelledByDay(device) {
 function fetchDistTravelledByHour(device, dateRange) {
     var start = dateRange[0].getFullYear() + "/" + (dateRange[0].getMonth() + 1) + "/" + dateRange[0].getDate();
     var end = dateRange[1].getFullYear() + "/" + (dateRange[1].getMonth() + 1) + "/" + dateRange[1].getDate();
-    console.log("fetching data from " + start + " to " + end);
     var query = "WITH distance_view AS (SELECT date_time, ST_Distance_Sphere(the_geom,lag(the_geom,1) OVER(ORDER BY device_info_serial, date_time)) AS distance_in_meters FROM bird_tracking WHERE device_info_serial='" + device + "' AND userflag IS FALSE AND date_time>'" + start + "' AND date_time<'" + end + "') SELECT extract(epoch FROM date_trunc('hour',date_time)) AS timestamp, round((sum(distance_in_meters)/1000)::numeric, 3) AS distance FROM distance_view GROUP BY timestamp ORDER BY timestamp";
     var url = "https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + query;
     console.log(url);
@@ -85,6 +84,7 @@ var app = function() {
     var yearcal;
     var yeardata;
     var yearcalRange;
+    var monthcal;
     var map;
     var cartodbLayer = "empty";
     var timestampFirstDate;
@@ -345,6 +345,12 @@ var app = function() {
                 }
             }
         });
+    }
+
+    // fetch data and create month chart
+    function createMonthChart() {
+        var bird = birds[selectedBird];
+        monthDataCall = fetchDistTravelledByHour(bird.device_info_serial, month);
     }
 
 }();
