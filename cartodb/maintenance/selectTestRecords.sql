b70705c8b88fa162e1c2f9eac5a3be6982637db7
Peter Desmet <peter.desmet.work@gmail.com>
2015-04-22 17:19:09 +0200

    Update selectTestRecords.sql

--- cartodb/maintenance/selectTestRecords.sql
+++ cartodb/maintenance/selectTestRecords.sql
@@ -4,8 +4,8 @@
 select
   count(*)
 from
-bird_tracking_new_data as t
-left join bird_tracking_devices as d
+"bird-tracking".bird_tracking_new_data as t
+left join lifewatch.bird_tracking_devices as d
 on t.device_info_serial = d.device_info_serial
 where
   t.date_time < d.tracking_started_at
