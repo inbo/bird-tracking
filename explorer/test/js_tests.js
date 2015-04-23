142500d408126de483e02dd70eff5f6f9656870a
Bart Aelterman <bart.aelterman@gmail.com>
2015-04-23 08:35:50 +0200

    use app object in test

--- explorer/test/js_tests.js
+++ explorer/test/js_tests.js
@@ -5,7 +5,7 @@
 asyncTest("fetch 10 occurrences for device 703", 1, function() {
     var device = "703";
     var query = "SELECT date_time FROM bird_tracking WHERE device_info_serial='" + device + "' LIMIT 10";
-    var result = fetchTrackingData("https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + query);
+    var result = app.fetchTrackingData("https://lifewatch-inbo.cartodb.com/api/v2/sql?q=" + query);
     var expectedNROfRecords = 10;
     result.done(function (data) {
         equal(data.rows.length, expectedNROfRecords);
