f603228e80ec26dff3b2c6b703cf080f520263e8
Peter Desmet <peter.desmet.work@gmail.com>
2015-09-30 10:27:40 +0200

    Update selectTrackingPeriods.sql

--- cartodb/maintenance/selectTrackingPeriods.sql
+++ cartodb/maintenance/selectTrackingPeriods.sql
@@ -9,7 +9,7 @@ select
   max(t.date_time) as end_date,
   max(t.date_time)::date - min(t.date_time)::date as days,
   d.remarks
-from "bird-tracking".bird_tracking_new_data t
+from lifewatch.bird_tracking_new_data t
   left join lifewatch.bird_tracking_devices d
   on t.device_info_serial = d.device_info_serial
 where t.userflag is false
