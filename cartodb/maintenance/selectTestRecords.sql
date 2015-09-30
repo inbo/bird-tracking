1ec9e8822fa0ac0193e991e4a0abcb3dd0b95273
Peter Desmet <peter.desmet.work@gmail.com>
2015-09-30 14:05:31 +0200

    Update selectTestRecords.sql

--- cartodb/maintenance/selectTestRecords.sql
+++ cartodb/maintenance/selectTestRecords.sql
@@ -1,11 +1,11 @@
 -- SQL to show number of records recorded earlier than tracking_started_at timestamp.
 -- Those records should be removed.
 
-select
+SELECT
   count(*)
-from
-"bird-tracking".bird_tracking_new_data as t
-left join lifewatch.bird_tracking_devices as d
-on t.device_info_serial = d.device_info_serial
-where
+FROM
+  lifewatch.bird_tracking_new_data as t
+  LEFT JOIN lifewatch.bird_tracking_devices as d
+  ON t.device_info_serial = d.device_info_serial
+WHERE
   t.date_time < d.tracking_started_at
