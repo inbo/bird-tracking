5ca8a781019285a1535b885a768a920411508efb
Peter Desmet <peter.desmet.work@gmail.com>
2015-09-30 14:06:25 +0200

    Update selectTrackingPeriods.sql

--- cartodb/maintenance/selectTrackingPeriods.sql
+++ cartodb/maintenance/selectTrackingPeriods.sql
@@ -1,21 +1,21 @@
 -- SQL to show tracking days per device.
 -- A relatively low number of tracking days could indicate that the bird is dead.
 
-select
+SELECT
   d.bird_name,
   d.scientific_name,
   d.colour_ring_code,
-  min(t.date_time) as start_date,
-  max(t.date_time) as end_date,
+  min(t.date_time) AS start_date,
+  max(t.date_time) AS end_date,
   max(t.date_time)::date - min(t.date_time)::date as days,
   d.remarks
-from lifewatch.bird_tracking_new_data t
-  left join lifewatch.bird_tracking_devices d
-  on t.device_info_serial = d.device_info_serial
-where t.userflag is false
-group by 
+FROM lifewatch.bird_tracking_new_data t
+  LEFT JOIN lifewatch.bird_tracking_devices d
+  ON t.device_info_serial = d.device_info_serial
+WHERE t.userflag is false
+GROUP BY 
   d.bird_name,
   d.scientific_name,
   d.colour_ring_code,
   d.remarks
-order by days desc
+ORDER BY days DESC
