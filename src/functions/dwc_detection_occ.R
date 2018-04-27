e7bebe2bb35c4ad7644b5a7f26f723efcd94b3e2
Peter Desmet <peter.desmet.work@gmail.com>
2018-04-27 10:51:35 +0200

    Start function for dwc mapping using Dplyr

---
+++ src/functions/dwc_detection_occ.R
@@ -0,0 +1,27 @@
+dwc_detection_occ <- function(individual_detections, metadata) {
+  require(dplyr)
+
+  input_colnames <- colnames(individual_detections)
+
+  occ <- individual_detections %>% mutate(
+    occurrenceID = "",
+
+    type = "Event",
+    language = "en",
+    license = metadata$license,
+    rightsHolder = metadata$rightsHolder,
+    accessRights = metadata$accessRights,
+    datasetID = metadata$datasetID,
+    institutionCode = metadata$institutionCode,
+    datasetName = metadata$datasetName,
+    basisOfRecord = "MachineObservation",
+    informationWithheld = metadata$informationWithheld,
+    # dynamicProperties
+
+    # sex
+  )
+
+  occ <- occ %>% select(-one_of(input_colnames))
+
+  return(occ)
+}
