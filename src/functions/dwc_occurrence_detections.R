3dbfff34b9c13ccec5bf72b077ab34c0b9734f92
Peter Desmet <peter.desmet.work@gmail.com>
2018-05-03 20:16:30 +0200

    Add quality flag for outliers

--- src/functions/dwc_occurrence_detections.R
+++ src/functions/dwc_occurrence_detections.R
@@ -1,8 +1,22 @@
 dwc_occurrence_detections <- function(individual_detections, metadata) {
   require(dplyr)
 
+  # Add quality issue flag
+  individual_detections <- individual_detections %>% mutate(
+    quality_issue = case_when(
+      speed_2d < 0 ~ TRUE,
+      speed_2d > 33.33333 ~ TRUE,
+      altitude > 10000 ~ TRUE,
+      h_accuracy > 1000 ~ TRUE,
+      date_time > Sys.Date() ~ TRUE,
+      TRUE ~ FALSE
+    )
+  )
+
+  # Save original column names
   input_colnames <- colnames(individual_detections)
 
+  # Map to Darwin Core
   occ <- individual_detections %>% mutate(
     occurrenceID = "",
 
@@ -19,6 +33,15 @@ dwc_occurrence_detections <- function(individual_detections, metadata) {
     # dynamicProperties
 
     # sex
+    occurrenceStatus = case_when(
+      quality_issue ~ "doubtful",
+      TRUE ~ "present"
+    ),
+    occurrenceRemarks = case_when(
+      quality_issue ~ "record flagged as doubtful",
+      TRUE ~ ""
+    ),
+
   )
 
   occ <- occ %>% select(-one_of(input_colnames))
