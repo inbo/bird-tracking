52d5daeab38450c72a3b3fc32ff34b2267b612b6
Peter Desmet <peter.desmet.work@gmail.com>
2018-05-03 20:34:56 +0200

    Indent SQL

--- sql/individuals_and_track_sessions.sql
+++ sql/individuals_and_track_sessions.sql
@@ -17,19 +17,22 @@ SELECT
   s.remarks AS track_session_remarks,
   s.track_session_id,
   s.tracker_id
-FROM (
-  SELECT * FROM gps.ee_individual_limited
-  UNION
-  SELECT * FROM gps.ee_shared_individual_limited) i
-LEFT JOIN (
-  SELECT * FROM gps.ee_track_session_limited
-  UNION
-  SELECT * FROM gps.ee_shared_track_session_limited) s
-  ON i.individual_id = s.individual_id
-LEFT JOIN gps.ee_species_limited sp
-  ON i.species_latin_name = sp.latin_name
-LEFT JOIN gps.ee_project_limited p
-  on s.key_name = p.key_name
+FROM
+  (
+    SELECT * FROM gps.ee_individual_limited
+    UNION
+    SELECT * FROM gps.ee_shared_individual_limited
+  ) i
+  LEFT JOIN (
+    SELECT * FROM gps.ee_track_session_limited
+    UNION
+    SELECT * FROM gps.ee_shared_track_session_limited
+  ) s
+    ON i.individual_id = s.individual_id
+  LEFT JOIN gps.ee_species_limited sp
+    ON i.species_latin_name = sp.latin_name
+  LEFT JOIN gps.ee_project_limited p
+    ON s.key_name = p.key_name
 WHERE
   s.key_name IN ({project_keys})
 ORDER BY
