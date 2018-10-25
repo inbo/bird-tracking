c67506bbff575b388f12301d7dcc612d4903b595
Peter Desmet <peter.desmet.work@gmail.com>
2018-10-26 01:07:47 +0200

    Update some comments

--- sql/movebank_reference_data.sql
+++ sql/movebank_reference_data.sql
@@ -1,13 +1,15 @@
 /* Created by Peter Desmet (INBO)
  *
- * Query maps UvA-BiTS DB fields to the Movebank standard reference data format
- * For an overview of Movebank terms, see https://www.movebank.org/node/2381
- * Order of terms is based on https://www.movebank.org/movebank/Movebank-reference-data-template.xlsx
+ * This query maps UvA-BiTS DB fields to Movebank attributes for metadata:
+ * https://www.movebank.org/node/2381#metadata
+ *
+ * Order of terms is based on Movebank standard reference data format template:
+ * https://www.movebank.org/movebank/Movebank-reference-data-template.xlsx
  */
 
 SELECT
   s.key_name AS project,--                          not a Movebank field, but included for reference
-  s.device_info_serial AS "tag-id",--               device_info_serial more widely used than tracker_id,
+  s.device_info_serial AS "tag-id"--                device_info_serial more widely used than tracker_id,
   i.ring_number AS "animal-id",--                   ring_number more widely used than individual_id
   s.track_session_id AS "deployment-id",
   i.species_latin_name AS "animal-taxon",
@@ -31,16 +33,16 @@ SELECT
     WHEN {animal_name_in_remarks} THEN i.remarks--  if TRUE, get animal nickname from i.remarks
     ELSE NULL
   END AS "animal-nickname",--                       not in Movebank-reference-data-template, but is available in Movebank database
-  {animal_life_stage} AS "animal-life-stage",--     not available in DB, likely "adult"
+  {animal_life_stage} AS "animal-life-stage",--     not available in DB: likely "adult"
   i.mass AS "animal-mass",
 -- "animal-reproductive-condition"                  not available in DB
-  {attachment_type} AS "attachment-type",--         not available in DB, likely "harness" or "other" (for leg loops)
--- "behavior-according-to"                          behavioral categories not available/uploaded to Movebank, could be supported in future
--- "data-processing-software"                       not applicable, locations are in raw sensor data
--- "deploy-off-person"                              person who removed tag, not available in DB
+  {attachment_type} AS "attachment-type",--         not available in DB: likely "harness" or "other" (for leg loops)
+-- "behavior-according-to"                          not available in DB: potentially supported in future
+-- "data-processing-software"                       not applicable: locations are in raw sensor data
+-- "deploy-off-person"                              not available in DB: person who removed tag
 -- "deploy-off-latitude"                            not available in DB
 -- "deploy-off-longitude"                           not available in DB
--- "deploy-on-person"                               person who attached tag, not available in DB
+-- "deploy-on-person"                               not available in DB: person who attached tag
 -- "deploy-on-latitude"                             is s.start_latitude, but Movebank term is primarily intended when sensor does not record precise positions
 -- "deploy-on-longitude"                            is s.start_longitude, but see s.start_latitude
   s.remarks AS "deployment-comments",
@@ -49,25 +51,25 @@ SELECT
     WHEN lower(s.remarks) LIKE '%dead%' THEN 'dead'-- track session remarks contains word "dead"
     ELSE NULL--                                     other values from Movebank controlled list cannot be consistently derived
   END AS "deployment-end-type",
--- "duty-cycle"                                     tags do have recording settings, but can change over time and not available in DB
+-- "duty-cycle"                                     not available in DB and variable over time
 -- "geolocator-calibration"                         not applicable
 -- "geolocator-light-threshold"                     not applicable
 -- "geolocator-sensor-comments"                     not applicable
 -- "geolocator-sun-elevation-angle"                 not applicable
--- "habitat-according-to"                           habitat information not available/uploaded to Movebank, could be supported in future
+-- "habitat-according-to"                           habitat information not available/uploaded to Movebank, potentially supported in future
   'provided by the GPS unit' AS "location-accuracy-comments",-- refers to e.g. h_accuracy recorded by tag
 -- "manipulation-comments"                          not available in DB and mostly not applicable
-  {manipulation_type} AS "manipulation-type",--     not available in DB, likely "none"
+  {manipulation_type} AS "manipulation-type",--     not available in DB: likely "none"
   p.station_name AS "study-site",--                 can be quite broad, but consistently populated in DB
   'other wireless' AS "tag-readout-method",--       zigbee two-way radio transceiver via antenna
--- "beacon-frequency"                               not applicable, for radio tags/retrieval beacon
+-- "beacon-frequency"                               not applicable: for radio tags/retrieval beacon
 -- "tag-comments"                                   s.remarks can contain this type of information, but unstructured, see "deployment-remarks" instead
 -- "tag-failure-comments"                           s.remarks can contain this type of information, but unstructured, see "deployment-remarks" instead
   'University of Amsterdam Bird Tracking System (UvA-BiTS)' AS "tag-manufacturer-name",
   t.mass AS "tag-mass",
--- "tag-model"                                      not available in DB, firmware version not a good substitute
+-- "tag-model"                                      not available in DB: firmware version not a good substitute
 -- "tag-processing-type"                            not applicable
--- "tag-production-date"                            not available in DB, firmware version not a good substitute
+-- "tag-production-date"                            not available in DB: firmware version not a good substitute
   s.device_info_serial AS "tag-serial-no"
 FROM
 -- individuals
