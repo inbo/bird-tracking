1658bc298ba9b9530ca683eaec2a7ea3991dba0a
Peter Desmet <peter.desmet.work@gmail.com>
2019-09-30 16:50:47 +0200

    Use better abbreviations for table names
    gps: detections
    ses: session
    ind: individual
    tag: tag

--- sql/movebank_gps.sql
+++ sql/movebank_gps.sql
@@ -1,6 +1,6 @@
 /* Created by Peter Desmet (INBO)
  *
- * This query maps UvA-BiTS DB fields to Movebank attributes for data:
+ * This query retrieves UvA-BiTS gps data in the Movebank gps data format:
  * https://www.movebank.org/node/2381#data
  *
  * It queries detections in ee_(shared_)tracking_speed_limited that fall within
@@ -8,19 +8,19 @@
  *
  * The fields from that view that could not be mapped to Movebank are:
  *
- * t.x_speed                                        x ground speed measured by tag in m/s
- * t.y_speed                                        y ground speed measured by tag in m/s
- * t.z_speed                                        z ground speed measured by tag in m/s
- * t.speed_accuracy                                 accuracy measured by tag on those speeds
- * t.speed_3d                                       ground speed calculated from x_speed, y_speed and z_speed
- * t.location                                       not useful: postgreSQL geometry
- * t.altitude_agl                                   cannot be mapped: is recorded altitude minus reference digital elevation model
+ * gps.x_speed                                      x ground speed measured by tag in m/s
+ * gps.y_speed                                      y ground speed measured by tag in m/s
+ * gps.z_speed                                      z ground speed measured by tag in m/s
+ * gps.speed_accuracy                               accuracy measured by tag on those speeds
+ * gps.speed_3d                                     ground speed calculated from x_speed, y_speed and z_speed
+ * gps.location                                     not useful: postgreSQL geometry
+ * gps.altitude_agl                                 cannot be mapped: is recorded altitude minus reference digital elevation model
  */
 
 SELECT
-  s.key_name AS project,--                          not a Movebank field, but included for reference
-  s.device_info_serial AS "tag-id",
-  s.ring_number AS "animal-id",
+  ses.key_name AS project,--                        not a Movebank field, but included for reference
+  ses.device_info_serial AS "tag-id",
+  ses.ring_number AS "animal-id",
   -- "acceleration-axes"                            not applicable: acceleration might have different timestamp than fix
   -- "acceleration-raw-x"                           not applicable: see acceleration-axes
   -- "acceleration-raw-y"                           not applicable: see acceleration-axes
@@ -34,7 +34,7 @@ SELECT
   -- "algorithm-marked-outlier"                     not applicable: reserved for Movebank filters
   -- "barometric depth"                             not applicable
   -- "barometric-height"                            not available in DB: if pressure is measured (special tags) it is not converted to height
-  t.pressure/100 AS "barometric-pressure",--        measured in Pascal, converted to HPa
+  gps.pressure/100 AS "barometric-pressure",--      measured in Pascal, converted to HPa
   -- "battery-charge-percent"                       not available in DB
   -- "battery-charging-current"                     not available in DB
   -- "behavioural-classification"                   not available in DB: potentially supported in future
@@ -46,33 +46,33 @@ SELECT
   -- "geolocator-fix-type"                          not applicable
   -- "geolocator-rise"                              not applicable
   -- "geolocator-twilight3"                         not applicable
-  t.positiondop AS "gps-dop",
+  gps.positiondop AS "gps-dop",
   -- "gps-fix-type"                                 not available in DB
   -- "gps-fix-type-raw"                             not available in DB
   -- "gps-hdop"                                     not available in DB
   -- "gps-maximum-signal-strength"                  not available in DB
-  t.satellites_used AS "gps-satellite-count",
-  t.gps_fixtime AS "gps-time-to-fix",--             in seconds
+  gps.satellites_used AS "gps-satellite-count",
+  gps.gps_fixtime AS "gps-time-to-fix",--           in seconds
   -- "gps-vdop"                                     not available in DB
   speed_2d as "ground-speed",--                     ground speed calculated from x_speed and y_speed (both measured by tag), in m/s
   -- "gsm-mcc-mnc"                                  not available in DB
   -- "gsm-signal-strength"                          not available in DB
   -- "habitat"                                      not available in DB
   CASE
-    WHEN t.direction < 0 THEN 360 + t.direction--   direction measured by sensor, in degrees from north (0-360), so negative values have to be converted (e.g -178 = 182 = almost south)
-    ELSE t.direction
+    WHEN gps.direction < 0 THEN 360 + gps.direction--direction measured by sensor, in degrees from north (0-360), so negative values have to be converted (e.g -178 = 182 = almost south)
+    ELSE gps.direction
   END AS "heading",--
   -- "height-above-ellipsoid"                       not available in DB
-  t.altitude AS "height-above-mean-sea-level",--    defined in DB as "Altitude above sea level measured by GPS tag in meters"
+  gps.altitude AS "height-above-mean-sea-level",--  defined in DB as "Altitude above sea level measured by GPS tag in meters"
   -- "height-raw"                                   not available in DB
   -- "import-marked-outlier"                        not available in DB: for flagging with *automated* methods outside of Movebank, opted not to calculate as it is always opinionated
   -- "lat-lower"                                    not applicable
   -- "lat-upper"                                    not applicable
   -- "light-level"                                  not applicable
   -- "local-timestamp"                              not available in DB: won't calculate either
-  t.latitude AS "location-lat",--                   in decimal degrees
-  t.longitude AS "location-long",--                 in decimal degrees
-  t.h_accuracy AS "location-error-numerical",--     in meters, is *horizontal* error
+  gps.latitude AS "location-lat",--                 in decimal degrees
+  gps.longitude AS "location-long",--               in decimal degrees
+  gps.h_accuracy AS "location-error-numerical",--   in meters, is *horizontal* error
   -- "location-error-percentile"                    not applicable
   -- "location-error-text"                          not applicable
   -- "long-lower"                                   not applicable
@@ -81,7 +81,7 @@ SELECT
   -- "magnetic-field-raw-y"                         not available in DB
   -- "magnetic-field-raw-z"                         not available in DB
   CASE
-    WHEN t.userflag <> 0 THEN TRUE--                defined in DB as "Data flagged as unacceptable by user if not equal to 0."
+    WHEN gps.userflag <> 0 THEN TRUE--              defined in DB as "Data flagged as unacceptable by user if not equal to 0."
     ELSE FALSE--                                    including default values 0
   END AS "manually-marked-outlier",--               "may also include outliers identified using other methods": since method for setting userflag is unknown in DB, this fits definition
   -- "manually-marked-valid"                        not available in DB: userflag does not allow to explicitly set record as valid
@@ -98,14 +98,14 @@ SELECT
   -- "study-time-zone"                              not available in DB
   -- "tag-technical-specification"                  not necessary
   -- "tag-voltage"                                  not available in DB
-  t.temperature AS "temperature-external",--        in degrees Celcius and is not body temperature
+  gps.temperature AS "temperature-external",--      in degrees Celcius and is not body temperature
   -- "temperature-max"                              not available in DB
   -- "temperature-min"                              not available in DB
   -- "tilt-angle"                                   not applicable: see acceleration-axes
   -- "tilt-x"                                       not applicable: see acceleration-axes
   -- "tilt-y"                                       not applicable: see acceleration-axes
   -- "tilt-z"                                       not applicable: see acceleration-axes
-  t.date_time AT TIME ZONE 'utc' AS "timestamp",--  date format is yyyy-MM-dd'T'HH:mm:ss'Z'
+  gps.date_time AT TIME ZONE 'utc' AS "timestamp",--date format is yyyy-MM-dd'T'HH:mm:ss'Z'
   -- "transmission-timestamp"                       not available in DB
   -- "twilight"                                     not applicable
   -- "twilight-excluded"                            not applicable
@@ -115,7 +115,7 @@ SELECT
   -- "utm-easting"                                  not applicable
   -- "utm-northing"                                 not applicable
   -- "utm-zone"                                     not applicable
-  t.v_accuracy AS "vertical-error-numerical"--      in meters
+  gps.v_accuracy AS "vertical-error-numerical"--    in meters
   -- "visible"                                      not applicable: calculated Movebank value
   -- "waterbird-workshop-behavior"                  not applicable
   -- "waterbird-workshop-deployment-special-event"  not applicable
@@ -125,14 +125,14 @@ FROM
   -- track session for ring_number
   (
     SELECT * FROM gps.{`track_session_table`} WHERE ring_number = {ring_number}
-  ) AS s
+  ) AS ses
 
   -- gps
-  LEFT JOIN gps.{`tracking_speed_table`} AS t
-    ON t.device_info_serial = s.device_info_serial
-    AND t.date_time BETWEEN s.start_date AND s.end_date
+  LEFT JOIN gps.{`tracking_speed_table`} AS gps
+    ON gps.device_info_serial = ses.device_info_serial
+    AND gps.date_time BETWEEN ses.start_date AND ses.end_date
     -- Because some tracking sessions have no meaningful track_session_end_date,
     -- we'll use today's date to exclude erroneous records in the future
-    AND t.date_time <= current_date
+    AND gps.date_time <= current_date
 ORDER BY
-  t.date_time
+  gps.date_time
--- sql/movebank_ref.sql
+++ sql/movebank_ref.sql
@@ -1,7 +1,7 @@
 /* Created by Peter Desmet (INBO)
  *
- * This query maps UvA-BiTS DB fields to Movebank attributes for metadata:
- * https://www.movebank.org/node/2381#metadata
+ * This query retrieves UvA-BiTS individual, session and tracker data in the
+ * Movebank reference data format: https://www.movebank.org/node/2381#metadata
  *
  * Order of terms is based on Movebank standard reference data format template:
  * https://www.movebank.org/movebank/Movebank-reference-data-template.xlsx
@@ -12,52 +12,52 @@
  *
  * The fields from that view that could not be mapped to Movebank are:
  *
- * i.individual_id                                  internal id, opted to use ring_number instead
- * i.start_date                                     not relevant
- * i.end_date                                       not relevant
- * s.project_id                                     internal id, opted to use key_name instead
- * s.tracker_id / t.tracker.id                      internal id, opted to use device_info_serial insteal
- * t.firmware_version                               cannot be mapped
- * t.start_date                                     not relevant
- * t.end_date                                       not relevant
- * t.x_o                                            cannot be mapped: accelerometer calibration
- * t.x_s                                            cannot be mapped: accelerometer calibration
- * t.y_o                                            cannot be mapped: accelerometer calibration
- * t.y_s                                            cannot be mapped: accelerometer calibration
- * t.z_o                                            cannot be mapped: accelerometer calibration
- * t.z_s                                            cannot be mapped: accelerometer calibration
+ * ind.individual_id                                internal id, opted to use ring_number instead
+ * ind.start_date                                   not relevant
+ * ind.end_date                                     not relevant
+ * ses.project_id                                   internal id, opted to use key_name instead
+ * ses.tracker_id / tag.tracker.id                  internal id, opted to use device_info_serial insteal
+ * tag.firmware_version                             cannot be mapped
+ * tag.start_date                                   not relevant
+ * tag.end_date                                     not relevant
+ * tag.x_o                                          cannot be mapped: accelerometer calibration
+ * tag.x_s                                          cannot be mapped: accelerometer calibration
+ * tag.y_o                                          cannot be mapped: accelerometer calibration
+ * tag.y_s                                          cannot be mapped: accelerometer calibration
+ * tag.z_o                                          cannot be mapped: accelerometer calibration
+ * tag.z_s                                          cannot be mapped: accelerometer calibration
  */
 
 SELECT
-  s.key_name AS project,--                          not a Movebank field, but included for reference
+  ses.key_name AS project,--                        not a Movebank field, but included for reference
 
   -- animals
-  i.remarks AS "animal-comments",--                 can contain animal name
+  ind.remarks AS "animal-comments",--               can contain animal name
   -- "animal-death-comments"                        not consistently available and expressible in DB
   -- "animal-earliest-date-born"                    not available in DB
   -- "animal-exact-date-of-birth"                   not available in DB
-  i.ring_number AS "animal-id",--                   ring_number is more widely used to refer animal than individual_id
+  ind.ring_number AS "animal-id",--                 ring_number is more widely used to refer animal than individual_id
   -- "animal-latest-date-born"                      not available in DB
   CASE
-    WHEN {bird_remarks_is_nickname} THEN i.remarks--  if TRUE, get animal nickname from i.remarks
+    WHEN {bird_remarks_is_nickname} THEN ind.remarks--if TRUE, get animal nickname from ind.remarks
     ELSE NULL
   END AS "animal-nickname",
   CASE
-    WHEN i.colour_ring IN
+    WHEN ind.colour_ring IN
       ('-', 'NA', 'None', 'none', '0000')
     THEN NULL--                                     colour_ring is a required field, so users resort to variations to express no ring
-    ELSE i.colour_ring
+    ELSE ind.colour_ring
   END AS "animal-ring-id",--                        opted to include colour_ring here, as it is not included elsewhere. ring_number = animal-id
   CASE
-    WHEN i.sex = 'X' THEN NULL--                    not possible to express this in Movebank controlled list
-    ELSE lower(i.sex)
+    WHEN ind.sex = 'X' THEN NULL--                  not possible to express this in Movebank controlled list
+    ELSE lower(ind.sex)
   END AS "animal-sex",
-  i.species_latin_name AS "animal-taxon",
+  ind.species_latin_name AS "animal-taxon",
   -- "animal-taxon-detail"                          not necessary, species_latin_name is expected to be supported in ITIS
 
   -- deployments
   {animal_life_stage} AS "animal-life-stage",--     not available in DB: likely "adult"
-  i.mass AS "animal-mass",
+  ind.mass AS "animal-mass",
   -- "animal-reproductive-condition"                not available in DB
   {attachment_type} AS "attachment-type",--         not available in DB: likely "harness" or "other" (for leg loops)
   -- "behavior-according-to"                        not available in DB
@@ -66,20 +66,20 @@ SELECT
   -- "deploy-off-longitude"                         not available in DB
   -- "deploy-off-person"                            not available in DB: person who removed tag
   CASE
-    WHEN s.end_date = '9999-12-31' THEN NULL--     year 9999 not accepted by Movebank, better to set to undefined
-    ELSE s.end_date AT TIME ZONE 'utc'--            some end_dates will still be set in (near) future
+    WHEN ses.end_date = '9999-12-31' THEN NULL--    year 9999 not accepted by Movebank, better to set to undefined
+    ELSE ses.end_date AT TIME ZONE 'utc'--          some end_dates will still be set in (near) future
   END AS "deploy-off-timestamp",
-  s.start_latitude AS "deploy-on-latitude",
-  s.start_longitude AS "deploy-on-longitude",
+  ses.start_latitude AS "deploy-on-latitude",
+  ses.start_longitude AS "deploy-on-longitude",
   -- "deploy-on-person"                             not available in DB: person who attached tag
-  s.start_date AT TIME ZONE 'utc' AS "deploy-on-timestamp",
-  s.remarks AS "deployment-comments",
-  -- "deployment-end-comments"                      s.remarks can contain this type of information, but unstructured, see "deployment-remarks" instead
+  ses.start_date AT TIME ZONE 'utc' AS "deploy-on-timestamp",
+  ses.remarks AS "deployment-comments",
+  -- "deployment-end-comments"                      ses.remarks can contain this type of information, but unstructured, see "deployment-remarks" instead
   CASE
-    WHEN lower(s.remarks) LIKE '%dead%' THEN 'dead'-- track session remarks contains word "dead"
+    WHEN lower(ses.remarks) LIKE '%dead%' THEN 'dead'-- track session remarks contains word "dead"
     ELSE NULL--                                     other values from Movebank controlled list cannot be consistently derived
   END AS "deployment-end-type",
-  s.track_session_id AS "deployment-id",
+  ses.track_session_id AS "deployment-id",
   -- "duty-cycle"                                   not available in DB and variable over time
   -- "geolocator-calibration"                       not applicable
   -- "geolocator-light-threshold"                   not applicable
@@ -89,46 +89,46 @@ SELECT
   'provided by the GPS unit' AS "location-accuracy-comments",-- refers to e.g. h_accuracy recorded by tag
   -- "manipulation-comments"                        not available in DB and mostly not applicable
   {manipulation_type} AS "manipulation-type",--     not available in DB: likely "none"
-  s.key_name AS "study-site",--                     project.station_name would have been slightly more human readable, but not accessible for shared projects
+  ses.key_name AS "study-site",--                   project.station_name would have been slightly more human readable, but not accessible for shared projects
   'other wireless' AS "tag-readout-method",--       zigbee two-way radio transceiver via antenna
 
   -- tags
   -- "beacon-frequency"                             not applicable: for radio tags/retrieval beacon
   'GPS' AS "sensor-type",
-  -- "tag-comments"                                 s.remarks can contain this type of information, but unstructured, see "deployment-comments" instead
-  -- "tag-failure-comments"                         s.remarks can contain this type of information, but unstructured, see "deployment-comments" instead
-  s.device_info_serial AS "tag-id",--               device_info_serial more widely used than tracker_id,
+  -- "tag-comments"                                 ses.remarks can contain this type of information, but unstructured, see "deployment-comments" instead
+  -- "tag-failure-comments"                         ses.remarks can contain this type of information, but unstructured, see "deployment-comments" instead
+  ses.device_info_serial AS "tag-id",--             device_info_serial more widely used than tracker_id,
   'University of Amsterdam Bird Tracking System (UvA-BiTS)' AS "tag-manufacturer-name",
-  t.mass AS "tag-mass",
+  tag.mass AS "tag-mass",
   -- "tag-model"                                    not available in DB: firmware version not a good substitute
   -- "tag-processing-type"                          not applicable
   -- "tag-production-date"                          not available in DB: firmware version not a good substitute
-  s.device_info_serial AS "tag-serial-no"
+  ses.device_info_serial AS "tag-serial-no"
 FROM
   -- individuals
   (
     SELECT * FROM gps.ee_individual_limited
     UNION
     SELECT * FROM gps.ee_shared_individual_limited
-  ) AS i
+  ) AS ind
 
   -- track sessions
   LEFT JOIN (
     SELECT * FROM gps.ee_track_session_limited
     UNION
     SELECT * FROM gps.ee_shared_track_session_limited
-  ) AS s
-    ON s.individual_id = i.individual_id
+  ) AS ses
+    ON ses.individual_id = ind.individual_id
 
   -- trackers
   LEFT JOIN (
     SELECT * FROM gps.ee_tracker_limited
     UNION
     SELECT * FROM gps.ee_shared_tracker_limited
-  ) AS t
-    ON t.device_info_serial = s.device_info_serial
+  ) AS tag
+    ON tag.device_info_serial = ses.device_info_serial
 WHERE
-  s.key_name = {project}
+  ses.key_name = {project}
 ORDER BY
   project,
   "deployment-id"
