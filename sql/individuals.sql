a87508166d1db7a65428355b516a772487b38e7b
Peter Desmet <peter.desmet.work@gmail.com>
2019-10-17 11:59:41 +0200

    Use longer names for table abbreviations

--- sql/individuals.sql
+++ sql/individuals.sql
@@ -1,38 +1,38 @@
 SELECT
-  s.key_name,
-  p.station_name,
-  i.species_latin_name,
-  sp.english_name,
-  i.individual_id,
-  i.ring_number,
-  i.colour_ring,
-  i.remarks AS individual_remarks,
-  i.mass,
-  i.sex,
-  s.track_session_id,
-  s.device_info_serial,
-  s.tracker_id,
-  s.start_date AT TIME ZONE 'utc' AS track_session_start_date,
-  s.end_date AT TIME ZONE 'utc' AS track_session_end_date,
-  s.start_latitude AS track_session_start_latitude,
-  s.start_longitude AS track_session_start_longitude,
-  s.remarks AS track_session_remarks
+  ses.key_name,
+  prj.station_name,
+  ind.species_latin_name,
+  spe.english_name,
+  ind.individual_id,
+  ind.ring_number,
+  ind.colour_ring,
+  ind.remarks AS individual_remarks,
+  ind.mass,
+  ind.sex,
+  ses.track_session_id,
+  ses.device_info_serial,
+  ses.tracker_id,
+  ses.start_date AT TIME ZONE 'utc' AS track_session_start_date,
+  ses.end_date AT TIME ZONE 'utc' AS track_session_end_date,
+  ses.start_latitude AS track_session_start_latitude,
+  ses.start_longitude AS track_session_start_longitude,
+  ses.remarks AS track_session_remarks
 FROM
   (
     SELECT * FROM gps.ee_individual_limited
     UNION
     SELECT * FROM gps.ee_shared_individual_limited
-  ) i
+  ) ind
   LEFT JOIN (
     SELECT * FROM gps.ee_track_session_limited
     UNION
     SELECT * FROM gps.ee_shared_track_session_limited
-  ) s
-    ON i.individual_id = s.individual_id
-  LEFT JOIN gps.ee_species_limited sp
-    ON i.species_latin_name = sp.latin_name
-  LEFT JOIN gps.ee_project_limited p
-    ON s.key_name = p.key_name
+  ) ses
+    ON ind.individual_id = ses.individual_id
+  LEFT JOIN gps.ee_species_limited spe
+    ON ind.species_latin_name = spe.latin_name
+  LEFT JOIN gps.ee_project_limited prj
+    ON ses.key_name = prj.key_name
 ORDER BY
-  key_name,
-  device_info_serial
+  ses.key_name,
+  ses.device_info_serial
