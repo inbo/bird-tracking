7be5d3720600b6a6f35079533fb73a924b164739
peterdesmet <peter.desmet@inbo.be>
2020-05-14 12:21:28 +0200

    Create function to mark outlier

--- src/functions/mark_outliers.R
+++ src/functions/mark_outliers.R
@@ -1,3 +1,43 @@
+#' Returns the Movebank gps data with a column `import-marked-outlier` that has
+#' `TRUE` if the speed between point `i` and `i-1` is above `max_speed`.
+#' `max_runs` indicates how often this process is repeated: recalculating speed
+#' after removing those already marked as outliers.
+mark_outliers <- function(x, max_speed = 30, max_runs = 3) {
+  outliers <- c()
+  gps_speed <- x
+  run <- 0
+
+  if (nrow(x) > 0) {
+    for (i in 1:max_runs) {
+      run <- run + 1
+      # Calculate speed (see function below)
+      gps_speed <- calc_speed(gps_speed)
+
+      # Record outliers: timestamps that have speed above max_speed
+      new_outliers <- gps_speed %>% filter(speed > max_speed) %>% pull(timestamp)
+
+      # Prepare next run if outliers have been found
+      if (length(new_outliers) == 0) {
+        break
+      } else {
+        outliers <- append(outliers, new_outliers)
+        # Remove outliers from dataframe for next run
+        gps_speed <- gps_speed %>% filter(!timestamp %in% outliers)
+      }
+    }
+    message(paste(length(outliers), "outliers found using", run, "runs."))
+  } else {
+    message("No data.")
+  }
+
+  # Add import-marked-outlier column
+  x <- x %>% mutate(`import-marked-outlier` = case_when(
+    timestamp %in% outliers ~ TRUE,
+    TRUE ~ FALSE
+  ))
+  return(x)
+}
+
 #' Returns gps data with column `speed`  in m/s between point `i` and `i-1`,
 #' using the trip package.
 calc_speed <- function(x, timestamp = "timestamp", lat = "location-lat",
